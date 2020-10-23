919f3fa12c000775d0bcfe7f4feaadc231214c29
Vuong Tuan Kiet <55266028+vuon0029@users.noreply.github.com>
2020-10-23 13:31:32 -0400

    Updates

---
+++ precache-manifest.f0047176204af9e2cd60bf41d5bc3dd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c215c06db07059029203cfa8559c78af",
+    "url": "/mad9135-react-gh-pages/index.html"
+  },
+  {
+    "revision": "87aaf689398b0dedbaae",
+    "url": "/mad9135-react-gh-pages/static/css/main.aa0726e7.chunk.css"
+  },
+  {
+    "revision": "ad8949f071fdfc1cbd99",
+    "url": "/mad9135-react-gh-pages/static/js/2.c8796422.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/mad9135-react-gh-pages/static/js/2.c8796422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87aaf689398b0dedbaae",
+    "url": "/mad9135-react-gh-pages/static/js/main.caaa015d.chunk.js"
+  },
+  {
+    "revision": "df10cf2aa4b037eabc78",
+    "url": "/mad9135-react-gh-pages/static/js/runtime-main.b143cde1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mad9135-react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
